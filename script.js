8abdd091dd698ca0145d2d9d0d7683dd156ccb07
TChristensenDrumz <tchristensendrumz@gmail.com>
2020-10-29 02:29:56 -0700

    Initialzed parameter arrays and added parameter and password functions

--- script.js
+++ script.js
@@ -10,5 +10,20 @@ function writePassword() {
 
 }
 
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "?", "/", "<", ">", ",", ".", "~"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var
+
+function getParameters(){
+  
+}
+
+function generatePassword() {
+  
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
